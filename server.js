7d8018ffc216811a8f71b88ca5fc3f35ebb09bbe
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-10-03 21:18:20 -0400

    Editing package.json, server and controllers

---
+++ server.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super duper secret',
+  cookie: {
+    maxAge: 900000,
+  },
+  resave: false,
+  rolling: true,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
+
+app.use(routes);
+
+hbs.handlebars.registerHelper('isContentOwnedBySessionUser', function(contentOwner, options) {
+  return (contentOwner == options.data.session_user_id) ? options.fn(this) : options.inverse(this);
+});
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `App listening on port ${PORT}! Go to http://localhost:${PORT} to create an account!`
+    )
+  );
+});
